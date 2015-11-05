081cd59f212c370e17b3aedd0b2cf8075945126c
Robinson Greig <robinsongreig@gmail.com>
2015-11-05 08:30:11 -0500

    add position and progress labels to racetrack view and update based on data

--- KardioKart/LeaderboardViewController.swift
+++ KardioKart/LeaderboardViewController.swift
@@ -23,7 +23,11 @@ class LeaderboardViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("leaderboardCell") as! LeaderboardTableViewCell
         if let user = self.users?[indexPath.row] {
-            cell.positionLabel.text = "\(indexPath.row + 1)."
+            let postfixDict: [Int: String] = [0: "th", 1: "st", 2: "nd", 3: "rd", 4: "th", 5: "th", 6: "th", 7: "th", 8: "th", 9: "th"]
+            let userPosition = indexPath.row + 1;
+            let userPositionLastDigit = userPosition % 10
+            let userPositionPostfix = postfixDict[userPositionLastDigit]!
+            cell.positionLabel.text = "\(userPosition)\(userPositionPostfix)"
             cell.stepLabel.text = String(user["stepCount"])
             cell.nameLabel.text = user["name"] as? String
         }
