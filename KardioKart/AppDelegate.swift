0e792d256c0e4ac1c03ac0ca44ac3d15c7816584
Bobby <bobbyren@gmail.com>
2016-08-03 21:34:53 -0400

    added crashlytics user info logging

--- KardioKart/AppDelegate.swift
+++ KardioKart/AppDelegate.swift
@@ -44,6 +44,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         Fabric.with([Crashlytics.self])
         
+        // TODO: Move this to where you establish a user session
+        self.logUser()
+
         return true
     }
 
@@ -132,6 +135,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         //self.revealController?.presentViewController(alert, animated: true, completion: nil)
     }
 
+    // MARK: - Analytics
+    func logUser() {
+        // TODO: Use the current user's information
+        // You can call any combination of these three methods
+//        Crashlytics.sharedInstance().setUserEmail("user@fabric.io")
+        if let user = PFUser.currentUser() {
+            Crashlytics.sharedInstance().setUserIdentifier(user.objectId)
+        }
+//        Crashlytics.sharedInstance().setUserName("Test User")
+    }
+    
     deinit {
         NSNotificationCenter.defaultCenter().removeObserver(self)
     }
