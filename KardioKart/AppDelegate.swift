a2954644a6520fdbfa9ee3acc61eeb6234339b26
Bobby <bobbyren@gmail.com>
2016-08-07 00:32:23 -0400

    added UIViewController utilities; background and foregrounding of app send notifications

--- KardioKart/AppDelegate.swift
+++ KardioKart/AppDelegate.swift
@@ -60,10 +60,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        NSNotificationCenter.defaultCenter().postNotificationName("app:to:background", object: nil, userInfo: nil)
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        NSNotificationCenter.defaultCenter().postNotificationName("app:to:foreground", object: nil, userInfo: nil)
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
